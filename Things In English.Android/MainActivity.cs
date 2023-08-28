34b9b37ea1df9d6726469cbfc2392720cfe2668b
theerudito <erudito.tv@gmail.com>
2023-08-28 18:23:35 -0500

    v1

--- Things In English.Android/MainActivity.cs
+++ Things In English.Android/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.Content.PM;
 using Android.Gms.Ads;
 using Android.OS;
 using Android.Runtime;
+using MarcTron.Plugin;
 
 namespace ThingsInEnglish.Droid
 {
@@ -11,9 +12,17 @@ namespace ThingsInEnglish.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+
             base.OnCreate(savedInstanceState);
+
             MobileAds.Initialize(ApplicationContext);
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
@@ -24,5 +33,7 @@ namespace ThingsInEnglish.Droid
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+
     }
 }
